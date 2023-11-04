d90c4551dd3c793b76d210fbdb336fed4d820ae7
adhasan7 <adsansapu@gmail.com>
2023-11-04 14:12:42 +0700

    upload3

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div>
-      <h2>Product</h2>
+      <h2 className='font-bold'>Product</h2>
       <p>lorem isummmmm mmmmm mmmmm</p>
     </div>
   );
